db24b914bc30e03dd9ba86c8dbbe688d3d4d360f
Savion Amerson <savionamerson@gmail.com>
2021-09-12 15:10:27 -0500

    Added annotations to code

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
+// some imports
 const axios = require("axios");
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
 
-const { Client, Intents, Message } = require("discord.js");
 
+// this part logs the bot into discord
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.login("ODc2MDMzMzQ3MjgzOTk2NzEy.YReLoQ.QOi4VdPszh2C5K3CkMatPkDbjws");
+client.login(`${process.env.TOKEN}`);
 
 client.once("ready", () => {
-  console.log("Ready!");
-  client.user.setActivity("1.3", { type: "VERSION"})
+  console.log("Ready!"); // this is here to tell me if the bot is online or not
+  client.user.setActivity("1.3", { type: "VERSION"}) // this line is kinda just for fun
 
-//   const guild = client.guilds.cache.get("851879640120426576");
-//   const channel = guild.channels.cache.get("883445508759257148");
+  const guild = client.guilds.cache.get("851879640120426576");  // tells the bot which server to go to with the server ID
+  const channel = guild.channels.cache.get("883445508759257148");  // tell the bot which channel to go to with the channel ID
 
-  const guild = client.guilds.cache.get("876033885589372948");
-  const channel = guild.channels.cache.get("876033885589372951");
+//   these are here for a bot testing server I created
+//   const guild = client.guilds.cache.get("876033885589372948");
+//   const channel = guild.channels.cache.get("876033885589372951");
 
 
+// now this is where the magic happens
 client.on("message", async (message) => {
     if (message.content === "!time") {
     
-    let getUserTime = async () => {
+    // this function tells axios to go into the world time API (this lil link below) and get the data
+    let getUserTime = async () => { 
         let response = await axios.get("https://worldtimeapi.org/api/timezone/America/New_York")
-        let userTime = response.data.datetime;
-        return userTime.slice(11, 16);
+        let userTime = response.data.datetime; // this line specifies which specific piece of data to grab (this API had more than just time)
+        return userTime.slice(11, 16); // this line cuts the string to display only the time. Without it, the output would be "2021-09-12T16:04:34.009381-04:00"
     };
 
-    let userTimeValue = await getUserTime();
+    // this part calls the function from above and sends the message to the channel
+    let userTimeValue = await getUserTime(); 
     console.log(userTimeValue);
     channel.send(`It is currently **${userTimeValue}** for Alyna :flag_us:`);
 
 
+    // rinse and repeat!
 
 
     let getUserTimeTwo = async () => {
@@ -48,6 +56,7 @@ client.on("message", async (message) => {
 
 
 
+
     let getUserTimeThree = async () => {
         let response = await axios.get("https://worldtimeapi.org/api/timezone/Europe/Paris")
         let userTime = response.data.datetime;
@@ -57,6 +66,7 @@ client.on("message", async (message) => {
     userTimeValue = await getUserTimeThree();
     console.log(userTimeValue);
     channel.send(`It is currently **${userTimeValue}** for Aqua :flag_fr:🤮`);
+
 
 
 
@@ -143,7 +153,6 @@ client.on("message", async (message) => {
 
 
 
-
     let getUserTimeTen = async () => {
         let response = await axios.get("https://worldtimeapi.org/api/timezone/America/Chicago")
         let userTime = response.data.datetime;
@@ -153,7 +162,6 @@ client.on("message", async (message) => {
     userTimeValue = await getUserTimeTen();
     console.log(userTimeValue);
     channel.send(`It is currently **${userTimeValue}** for omellettefoot (the second coolest 😎) :flag_us:`);
-
 
 
 
